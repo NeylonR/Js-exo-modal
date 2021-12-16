139dc7c241848186d8b7164ae115a0053fd10258
NeylonR <rodrigues.neylon.0@gmail.com>
2021-12-16 14:29:50 +0100

    Mis à jour

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,36 +1,52 @@
+// déclaration de variable lié au DOM
 const getButton = document.querySelectorAll("button");
 const getCard = document.querySelectorAll(".card");
 const getModal = document.getElementById("modal");
 const getModalContent = document.getElementById("modalContent");
-const getP = document.querySelector("#modal p");
-const getTitle = document.querySelector("#modal h1");
+const getPModal = document.querySelector("#modal p");
+const getTitleModal = document.querySelector("#modal h1");
 const getCloseButton = document.getElementById("closeButton");
+const getBgModal = document.getElementById("backgroundModal");
+
+let getCardOfParent;
+
+// fonction pour retirer le modal + background + l'effet boxshadow sur la card 
+const hideAction = function(){
+    getModal.classList.replace("modalShow","modalHidden");
+    getBgModal.classList.replace("show", "hidden");
+    getCardOfParent.classList.replace("cardColor", "cardNormal");
+}
 
 getButton.forEach(element => {
     // boucle foreach pour ajouter un eventlistener sur chaque button
     element.addEventListener("click", function() {
-        // si le modal est caché ( class ) je le rend visible et je change le titre et p du modal selon le h1 et p de l'element parent du button 
+        // je récupère toute la div parent du button
+        getCardOfParent = element.parentNode;
+        // si le modal est caché (class) je le rend visible, puis je remplace le titre H1 et P du modal par le H1 et P de l'element parent du button sur lequel le click a été effectué
         if(getModal.className == "modalHidden") {
-            let getH1 = element.parentElement.getElementsByTagName('h1');
-            let getPOfCard = element.parentElement.getElementsByTagName('p');
-            getTitle.innerText = getH1[0].innerText;
-            getP.innerHTML = `Rajout de texte : ${getPOfCard[0].innerText}`;
-            getModal.classList.replace("modalHidden",'modalShow');                    
+            let getH1 = element.parentElement.getElementsByTagName("h1");
+            let getPOfCard = element.parentElement.getElementsByTagName("p");
+            getTitleModal.innerText = getH1[0].innerText;
+            getPModal.innerText = `Rajout de texte : ${getPOfCard[0].innerText}`;
+            // rajout d'un box shadow sur la carte selectioné, apparition d'une div en background sur tout le body
+            getCardOfParent.classList.replace("cardNormal", "cardColor" );
+            getBgModal.classList.replace("hidden", "show");
+            getModal.classList.replace("modalHidden","modalShow");                    
         } else {
-            // s'il est déjà visible lors du click, je le recache
-            getModal.classList.replace('modalShow', "modalHidden");
+            // si pour une raison inconnu le modal est déjà visible lors du click sur le button, je le cache
+            hideAction();
         }  
     });
 });
 
 // la croix du modal pour cacher le modal
 getCloseButton.addEventListener("click", function() {
-    getModal.classList.replace( "modalShow","modalHidden");
+    hideAction();
 })
 
 window.addEventListener("click", function(e) {
-    // lors du click sur la page je vérifie qu'il ne soit pas sur un button/modal/le contenu du modal/son P et h1 et je cache le modal
-    if (e.target != getButton[0] && e.target != getButton[1] && e.target != getButton[2] && e.target != getButton[3] && e.target != getButton[4] && e.target != getButton[5] && e.target != getModalContent && e.target != getP && e.target != getTitle) {
-        getModal.classList.replace("modalShow","modalHidden");
+    // lors du click sur le background ( qui apparait lorsque le modal est visible ), ou sur la croix du modal, je cache tout
+    if (e.target == getBgModal || e.target == getCloseButton) {
+        hideAction();
     }
-  })
+})
